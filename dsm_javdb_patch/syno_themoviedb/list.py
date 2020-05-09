1774be1cb95d92f1021fdbdf33cc9d26c88e374a
crazykuma <crazykuma@qq.com>
2020-05-09 13:31:35 +0800

    fix:获取图片地址失败的问题

--- dsm_javdb_patch/syno_themoviedb/list.py
+++ dsm_javdb_patch/syno_themoviedb/list.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python2
-#-*- coding:utf8 -*-
-# By sanyle 2018-05-03
+# -*- coding:utf8 -*-
+# Created By sanyle 2018-05-03
+# Edited By crazykuma 2020-05-09
 
 import requests
-import re,sys,json,os
+import sys
+import json
 from bs4 import BeautifulSoup
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
 def javlist(title):
     url = "https://javdb4.com/search?f=all&q="
     r = requests.get(url+title)
     r = r.content.decode("utf-8")
-    soup = BeautifulSoup(r,"lxml")
-    list = soup.select("#videos .column")
-    json = {}
-    listnum = len(list)
-    json['total'] = listnum
+    soup = BeautifulSoup(r, "lxml")
+    res = soup.select("#videos .column")
+    result = {}
+    listnum = len(res)
+    result['total'] = listnum
     data = []
-    for i in range(len(list)):
-        if i>3:
+    for i in range(len(res)):
+        if i > 3:
             break
         vmsg = {}
-        id = list[i].select("a")[0].get('href').replace("/v/","")
-        title = list[i].select(".uid")[0].text.strip()
-        sub_title = list[i].select(".video-title")[0].text.strip()
-        poster = list[i].select(".item-image > img")[0].get("src")
-        if poster.find("http")==-1:
-            poster="https:"+poster
+        id = res[i].select("a")[0].get('href').replace("/v/", "")
+        title = res[i].select(".uid")[0].text.strip()
+        sub_title = res[i].select(".video-title")[0].text.strip()
+        poster = res[i].select(".item-image > img")[0].get("data-src")
+        if poster.find("http") == -1:
+            poster = "https:"+poster
         vmsg['id'] = id
         vmsg['title'] = title
         vmsg['sub_title'] = sub_title
@@ -37,13 +40,16 @@ def javlist(title):
         vmsg['poster'] = poster
         data.append(vmsg)
 
-    json['data'] = data
-    return json
+    result['data'] = data
+    return result
+
 
 def main(argv):
-    title = "".join(argv).replace('BD','').replace('HD','').replace('：',':')
-    list =  javlist(title)
-    data = json.dumps(list, ensure_ascii=False)
+    title = "".join(argv).replace('BD', '').replace('HD', '').replace('：', ':')
+    arr = javlist(title)
+    data = json.dumps(arr, ensure_ascii=False)
     print(data)
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
